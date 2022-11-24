423688395f75f4079582074a783e05f1ea5081c8
alastairfarman <106626671+alastairfarman@users.noreply.github.com>
2022-11-24 09:39:54 +0000

    Set-up before scroll script

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Video from './Video';
+
+function App() {
+  return (
+    <>
+      <div className='img'></div>
+<Video/>
+      <div className='overlay'></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Video.js
@@ -0,0 +1,13 @@
+export default function Video() {
+    return (
+        <><h1>FUCK</h1>
+<div className="video-wrapper">
+<video playsInline autoPlay muted id="myVideo">
+  <source src="A-FULL.mp4" type="video/mp4"/>
+</video>
+</div>
+<div className="logo"><img src="A-LOGO.png" alt=""></img></div>
+
+        </>
+    )
+}
